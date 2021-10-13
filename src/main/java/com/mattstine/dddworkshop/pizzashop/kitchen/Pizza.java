8e3f3b22fc3e5061f8d21e76ec5fd49d5376f184
Matt Stine <matt.stine@gmail.com>
2021-10-13 10:27:30 -0500

    [LAB 4] submission

--- src/main/java/com/mattstine/dddworkshop/pizzashop/kitchen/Pizza.java
+++ src/main/java/com/mattstine/dddworkshop/pizzashop/kitchen/Pizza.java
@@ -1,5 +1,6 @@
 package com.mattstine.dddworkshop.pizzashop.kitchen;
 
+import com.mattstine.dddworkshop.pizzashop.infrastructure.events.adapters.InProcessEventLog;
 import com.mattstine.dddworkshop.pizzashop.infrastructure.events.ports.EventLog;
 import com.mattstine.dddworkshop.pizzashop.infrastructure.events.ports.Topic;
 import com.mattstine.dddworkshop.pizzashop.infrastructure.repository.ports.Aggregate;
@@ -102,7 +103,12 @@ public final class Pizza implements Aggregate {
 
     @Override
     public Pizza identity() {
-        return null;
+        return Pizza.builder()
+                .eventLog(EventLog.IDENTITY)
+                .ref(PizzaRef.IDENTITY)
+                .size(Size.IDENTITY)
+                .kitchenOrderRef(KitchenOrderRef.IDENTITY)
+                .build();
     }
 
     @Override
@@ -136,6 +142,27 @@ public final class Pizza implements Aggregate {
 
         @Override
         public Pizza apply(Pizza pizza, PizzaEvent pizzaEvent) {
+            if (pizzaEvent instanceof PizzaAddedEvent) {
+                PizzaAddedEvent pae = (PizzaAddedEvent) pizzaEvent;
+                return Pizza.builder()
+                        .ref(pae.getRef())
+                        .size(pae.getState().getSize())
+                        .eventLog(InProcessEventLog.instance())
+                        .kitchenOrderRef(pae.getState().getKitchenOrderRef())
+                        .build();
+            } else if (pizzaEvent instanceof PizzaPrepStartedEvent) {
+                pizza.state = State.PREPPING;
+                return pizza;
+            }else if (pizzaEvent instanceof PizzaPrepFinishedEvent) {
+                pizza.state = State.PREPPED;
+                return pizza;
+            }else if (pizzaEvent instanceof PizzaBakeStartedEvent) {
+                pizza.state = State.BAKING;
+                return pizza;
+            }else if (pizzaEvent instanceof PizzaBakeFinishedEvent) {
+                pizza.state = State.BAKED;
+                return pizza;
+            }
             return null;
         }
     }
